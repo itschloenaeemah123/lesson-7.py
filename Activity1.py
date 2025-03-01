942d0d2ee95c756273661029ab21f8d9d21e1289
itschloenaeemah123 <owodoyin321@yahoo.com>
2025-03-01 18:24:35 +0100

    lesson6.py

---
+++ Activity1.py
@@ -0,0 +1,14 @@
+print (" for loops ")
+name ="Naeemah"
+print(name)
+for ch in name:
+    print(ch)
+
+fruits =["apple","banana","cherry","dragon","elder berry"] 
+for fruit in fruits:
+    print(fruit) 
+
+for i in range(1,11):  
+    print(i)
+for i in range(10,0,-1):
+    print(i) 
